153f07d2ce473d581bf9c8001d70973c7c070a8a
Praneeth83 <167945790+Praneeth83@users.noreply.github.com>
2025-09-04 11:15:18 +0530

    db-connection

---
+++ Backend-Test-Submission/config/db.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	dsn := "host=local user=postgres password=postgres dbname=urlshortner port=5432 sslmode=disable"
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database", err)
+	}
+	fmt.Println("Connected to postgres server using GORM")
+}
--- Backend-Test-Submission/main.go
+++ Backend-Test-Submission/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"Backend-Test-Submission/config"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	config.InitDB()
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, echo.Map{
